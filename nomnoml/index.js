e6697a81ed24e434b053b2bcebe54d7bdd21a683
Guillaume Grossetie <ggrossetie@gmail.com>
2019-01-20 17:48:22 +0100

    Build a tiny Docker image with nomnoml

---
+++ nomnoml/index.js
@@ -0,0 +1,23 @@
+const nomnoml = require('nomnoml');
+
+const src = `[Pirate|eyeCount: Int|raid();pillage()|
+  [beard]--[parrot]
+  [beard]-:>[foul mouth]
+]
+
+[<abstract>Marauder]<:--[Pirate]
+[Pirate]- 0..7[mischief]
+[jollyness]->[Pirate]
+[jollyness]->[rum]
+[jollyness]->[singing]
+[Pirate]-> *[rum|tastiness: Int|swig()]
+[Pirate]->[singing]
+[singing]<->[rum]
+
+[<start>st]->[<state>plunder]
+[plunder]->[<choice>more loot]
+[more loot]->[st]
+[more loot] no ->[<end>e]
+
+[<actor>Sailor] - [<usecase>shiver me;timbers]`;
+console.log(nomnoml.renderSvg(src));
