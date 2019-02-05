b284233b99741830a93cd18915ddbe761fa842a0
Guillaume Grossetie <ggrossetie@gmail.com>
2019-02-05 15:03:46 +0100

    Add Mermaid examples

--- blockdiag/examples.py
+++ blockdiag/examples.py
@@ -475,6 +475,51 @@ nomnoml_sample="""
 """
 sample('nomnoml', nomnoml_sample)
 
+# Mermaid
+section('Mermaid')
+mermaid_flowchart_sample="""
+graph TD;
+    A-->B;
+    A-->C;
+    B-->D;
+    C-->D;
+"""
+sample('mermaid', mermaid_flowchart_sample)
+
+mermaid_seqdiag_sample="""
+sequenceDiagram
+    participant Alice
+    participant Bob
+    Alice->John: Hello John, how are you?
+    loop Healthcheck
+        John->John: Fight against hypochondria
+    end
+    Note right of John: Rational thoughts <br/>prevail...
+    John-->Alice: Great!
+    John->Bob: How about you?
+    Bob-->John: Jolly good!
+"""
+sample('mermaid', mermaid_seqdiag_sample)
+
+mermaid_gant_sample="""
+gantt
+        dateFormat  YYYY-MM-DD
+        title Adding GANTT diagram functionality to mermaid
+        section A section
+        Completed task            :done,    des1, 2014-01-06,2014-01-08
+        Active task               :active,  des2, 2014-01-09, 3d
+        Future task               :         des3, after des2, 5d
+        Future task2               :         des4, after des3, 5d
+        section Critical tasks
+        Completed task in the critical line :crit, done, 2014-01-06,24h
+        Implement parser and jison          :crit, done, after des1, 2d
+        Create tests for parser             :crit, active, 3d
+        Future task in critical line        :crit, 5d
+        Create tests for renderer           :2d
+        Add to mermaid                      :1d
+"""
+sample('mermaid', mermaid_gant_sample)
+
 # UMlet
 section('UMlet')
 umlet_analysis = """
