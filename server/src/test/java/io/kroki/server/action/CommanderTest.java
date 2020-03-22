b9477a2216ed1bc543387c73473c5e82f9950a33
Guillaume Grossetie <ggrossetie@gmail.com>
2020-03-22 12:10:31 +0100

    Make sure there is no more data in the stdout stream

--- server/src/test/java/io/kroki/server/action/CommanderTest.java
+++ server/src/test/java/io/kroki/server/action/CommanderTest.java
@@ -6,11 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -52,6 +55,26 @@ class CommanderTest {
   }
 
   @Test
+  void should_convert_diagram() throws IOException, InterruptedException {
+    if (Files.isExecutable(Paths.get("/usr/bin/dot"))) {
+      String source = read("hello.dot");
+      byte[] result = new Commander(new JsonObject()).execute(source.getBytes(), "dot", "-T", "svg");
+      String output = new String(result);
+      assertThat(output.trim()).isEqualTo(read("hello.expected.svg").trim());
+    }
+  }
+
+  @Test
+  void should_convert_large_diagram() throws IOException, InterruptedException {
+    if (Files.isExecutable(Paths.get("/usr/bin/node"))) {
+      String source = read("./volcano.vega");
+      byte[] result = new Commander(new JsonObject()).execute(source.getBytes(), "node", "../vega/index.js", "--output-format", "svg");
+      String output = new String(result);
+      assertThat(output.trim()).isEqualTo(read("volcano.expected.svg").trim());
+    }
+  }
+
+  @Test
   void should_configure_the_timeout_in_milliseconds() {
     HashMap<String, Object> config = new HashMap<>();
     config.put("KROKI_COMMAND_TIMEOUT", "4000ms");
@@ -82,6 +105,11 @@ class CommanderTest {
     assertThatThrownBy(() -> new Commander(new JsonObject(config)))
       .isInstanceOf(IllegalArgumentException.class)
       .hasMessageStartingWith("Failed to parse environment variable 'KROKI_COMMAND_TIMEOUT' with value '4y' as a time value: unit is missing or unrecognized");
+  }
 
+  private String read(String name) throws IOException {
+    try (BufferedReader buffer = new BufferedReader(new InputStreamReader(CommanderTest.class.getClassLoader().getResourceAsStream(name)))) {
+      return buffer.lines().collect(Collectors.joining("\n"));
+    }
   }
 }
