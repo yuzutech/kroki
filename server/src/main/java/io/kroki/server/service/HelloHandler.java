bef7c02cebe6b25e156854ce1b18edd5745e2950
Guillaume Grossetie <ggrossetie@gmail.com>
2020-03-27 10:20:53 +0100

    Upgrade diagram libraries versions in the docs

--- server/src/main/java/io/kroki/server/service/HelloHandler.java
+++ server/src/main/java/io/kroki/server/service/HelloHandler.java
@@ -42,7 +42,7 @@ public class HelloHandler {
       // QUESTION: should we dynamically fetch the versions ?
       serviceVersions.add(new ServiceVersion("actdiag", "2.0.0"));
       serviceVersions.add(new ServiceVersion("blockdiag", "2.0.1"));
-      serviceVersions.add(new ServiceVersion("c4plantuml", "1.2020.3"));
+      serviceVersions.add(new ServiceVersion("c4plantuml", "1.2020.4"));
       serviceVersions.add(new ServiceVersion("ditaa", "1.3.13"));
       serviceVersions.add(new ServiceVersion("erd", "0.1.3.0"));
       serviceVersions.add(new ServiceVersion("graphviz", "2.40.1"));
@@ -50,13 +50,13 @@ public class HelloHandler {
       serviceVersions.add(new ServiceVersion("nomnoml", "0.6.2"));
       serviceVersions.add(new ServiceVersion("nwdiag", "2.0.0"));
       serviceVersions.add(new ServiceVersion("packetdiag", "2.0.0"));
-      serviceVersions.add(new ServiceVersion("plantuml", "1.2020.3"));
+      serviceVersions.add(new ServiceVersion("plantuml", "1.2020.4"));
       serviceVersions.add(new ServiceVersion("rackdiag", "2.0.0"));
       serviceVersions.add(new ServiceVersion("seqdiag", "2.0.0"));
       serviceVersions.add(new ServiceVersion("svgbob", "0.4.2"));
       serviceVersions.add(new ServiceVersion("umlet", "14.3.0"));
       serviceVersions.add(new ServiceVersion("vega", "5.10.0"));
-      serviceVersions.add(new ServiceVersion("vegalite", "4.7.0"));
+      serviceVersions.add(new ServiceVersion("vegalite", "4.8.1"));
       serviceVersions.add(new ServiceVersion("wavedrom", "2.3.2"));
       String versionsTable = generateVersionsTable(serviceVersions);
       routingContext
