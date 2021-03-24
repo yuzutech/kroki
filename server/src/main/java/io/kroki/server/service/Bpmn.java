03910fbe65cf71750fd6e7875ce228bed0e17d83
Guillaume Grossetie <ggrossetie@gmail.com>
2021-03-24 21:57:51 +0100

    Update versions

--- server/src/main/java/io/kroki/server/service/Bpmn.java
+++ server/src/main/java/io/kroki/server/service/Bpmn.java
@@ -47,7 +47,7 @@ public class Bpmn implements DiagramService {
 
   @Override
   public String getVersion() {
-    return "8.2.0";
+    return "8.2.2";
   }
 
   @Override
--- server/src/test/java/io/kroki/server/service/HealthServiceTest.java
+++ server/src/test/java/io/kroki/server/service/HealthServiceTest.java
@@ -26,7 +26,7 @@ public class HealthServiceTest {
     List<ServiceVersion> serviceVersions = healthHandler.getServiceVersions();
     assertThat(krokiBuildHash).isNotEmpty();
     assertThat(krokiVersionNumber).isNotEmpty();
-    assertThat(serviceVersions).contains(new ServiceVersion("plantuml", "1.2021.1"));
+    assertThat(serviceVersions).contains(new ServiceVersion("plantuml", "1.2021.3"));
   }
 
   @Test
