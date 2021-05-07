3f50dba8e2ba05a32df74ef5e060c23e81fbf0e1
Guillaume Grossetie <ggrossetie@gmail.com>
2021-05-07 20:28:19 +0200

    Add test to avoid regression

--- server/src/test/java/io/kroki/server/service/C4PlantumlServiceTest.java
+++ server/src/test/java/io/kroki/server/service/C4PlantumlServiceTest.java
@@ -1,5 +1,6 @@
 package io.kroki.server.service;
 
+import io.kroki.server.format.FileFormat;
 import io.kroki.server.security.SafeMode;
 import org.junit.jupiter.api.Test;
 
@@ -36,5 +37,36 @@ public class C4PlantumlServiceTest {
       "@enduml\n";
     String result = Plantuml.sanitize(diagram, SafeMode.UNSAFE);
     assertThat(result).isEqualTo(diagram);
+  }
+
+  @Test
+  void should_not_use_network() throws IOException {
+    String diagram = "@startuml\n" +
+      "!include C4_Context.puml\n" +
+      "\n" +
+      "title System Context diagram for Internet Banking System\n" +
+      "\n" +
+      "Person(customer, \"Banking Customer\", \"A customer of the bank, with personal bank accounts.\")\n" +
+      "System(banking_system, \"Internet Banking System\", \"Allows customers to check their accounts.\")\n" +
+      "\n" +
+      "System_Ext(mail_system, \"E-mail system\", \"The internal Microsoft Exchange e-mail system.\")\n" +
+      "System_Ext(mainframe, \"Mainframe Banking System\", \"Stores all of the core banking information.\")\n" +
+      "\n" +
+      "Rel(customer, banking_system, \"Uses\")\n" +
+      "Rel_Back(customer, mail_system, \"Sends e-mails to\")\n" +
+      "Rel_Neighbor(banking_system, mail_system, \"Sends e-mails\", \"SMTP\")\n" +
+      "Rel(banking_system, mainframe, \"Uses\")\n" +
+      "@enduml";
+    // non existing proxy!
+    System.setProperty("socksProxyHost", "127.0.0.1");
+    System.setProperty("socksProxyPort", "1234");
+    try {
+      // should not use the network!
+      byte[] convert = Plantuml.convert(Plantuml.sanitize(diagram, SafeMode.SAFE), FileFormat.SVG);
+      assertThat(convert).isNotEmpty();
+    } finally {
+      System.clearProperty("socksProxyHost");
+      System.clearProperty("socksProxyPort");
+    }
   }
 }
