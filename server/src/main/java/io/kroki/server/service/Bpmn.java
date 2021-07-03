389891cdf35c76df9007344da8cf54c4c565823a
Guillaume Grossetie <ggrossetie@gmail.com>
2021-07-03 15:06:43 +0200

    Upgrade dependency versions

--- server/src/main/java/io/kroki/server/service/Bpmn.java
+++ server/src/main/java/io/kroki/server/service/Bpmn.java
@@ -47,7 +47,7 @@ public class Bpmn implements DiagramService {
 
   @Override
   public String getVersion() {
-    return "8.6.0";
+    return "8.7.1";
   }
 
   @Override
--- server/src/main/java/io/kroki/server/service/Mermaid.java
+++ server/src/main/java/io/kroki/server/service/Mermaid.java
@@ -47,7 +47,7 @@ public class Mermaid implements DiagramService {
 
   @Override
   public String getVersion() {
-    return "8.10.1";
+    return "8.11.0";
   }
 
   @Override
