a61b1cbc2199d01e5659f36d902cd77b8d569646
Guillaume Grossetie <ggrossetie@gmail.com>
2021-09-09 09:05:29 +0200

    Remove XML comments to make tests reproducible

--- server/src/test/java/io/kroki/server/service/StructurizrServiceTest.java
+++ server/src/test/java/io/kroki/server/service/StructurizrServiceTest.java
@@ -21,15 +21,7 @@ public class StructurizrServiceTest {
     String source = read("./gettingstarted.structurizr");
     String expected = read("./gettingstarted.expected.svg");
     byte[] result = Structurizr.convert(source, FileFormat.SVG, new StructurizrPlantUMLWriter(), new JsonObject());
-    assertThat(new String(result)).isEqualToIgnoringNewLines(expected);
-  }
-
-  @Test
-  public void should_convert_bigbank_example_first_view() throws IOException {
-    String source = read("./bigbank.structurizr");
-    String expected = read("./bigbank.first.expected.svg");
-    byte[] result = Structurizr.convert(source, FileFormat.SVG, new StructurizrPlantUMLWriter(), new JsonObject());
-    assertThat(new String(result)).isEqualToIgnoringNewLines(expected);
+    assertThat(stripComments(new String(result))).isEqualToIgnoringNewLines(expected);
   }
 
   @Test
@@ -39,7 +31,7 @@ public class StructurizrServiceTest {
     JsonObject options = new JsonObject();
     options.put("view-key", "Containers");
     byte[] result = Structurizr.convert(source, FileFormat.SVG, new StructurizrPlantUMLWriter(), options);
-    assertThat(new String(result)).isEqualToIgnoringNewLines(expected);
+    assertThat(stripComments(new String(result))).isEqualToIgnoringNewLines(expected);
   }
 
   @Test
@@ -49,7 +41,7 @@ public class StructurizrServiceTest {
     JsonObject options = new JsonObject();
     options.put("view-key", "SystemContext");
     byte[] result = Structurizr.convert(source, FileFormat.SVG, new StructurizrPlantUMLWriter(), options);
-    assertThat(new String(result)).isEqualToIgnoringNewLines(expected);
+    assertThat(stripComments(new String(result))).isEqualToIgnoringNewLines(expected);
   }
 
   @Test
@@ -68,6 +60,10 @@ public class StructurizrServiceTest {
     assertThatThrownBy(() -> Structurizr.convert(source, FileFormat.SVG, new StructurizrPlantUMLWriter(), new JsonObject()))
       .isInstanceOf(BadRequestException.class)
       .hasMessage("Empty diagram, does not have any view.");
+  }
+
+  private String stripComments(String xmlContent) {
+    return xmlContent.replaceAll("<!--[\\s\\S]*?-->", "");
   }
 
   private String read(String name) throws IOException {
