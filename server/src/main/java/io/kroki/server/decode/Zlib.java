4cfd3bd19b68fab79220cc35bbbde17f4d8b2298
Guillaume Grossetie <ggrossetie@gmail.com>
2021-09-30 08:49:30 +0200

    Fix the deflate implementation in Java (#912)

--- server/src/main/java/io/kroki/server/decode/Zlib.java
+++ server/src/main/java/io/kroki/server/decode/Zlib.java
@@ -8,17 +8,16 @@ import java.util.zip.InflaterInputStream;
 
 public class Zlib {
 
-  public static byte[] compress(byte[] source) throws IOException {
-    byte[] result = new byte[2048];
+  public static byte[] compress(byte[] source) {
     Deflater deflater = new Deflater(Deflater.BEST_COMPRESSION);
-    deflater.setInput(source, 0, source.length);
+    deflater.setInput(source);
     deflater.finish();
-    int compressedLength = deflater.deflate(result, 0, source.length, Deflater.FULL_FLUSH);
-    deflater.end();
-    try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream()) {
-      byteArrayOutputStream.write(result, 0, compressedLength);
-      return byteArrayOutputStream.toByteArray();
-    }
+
+    byte[] buffer = new byte[2048];
+    int compressedLength = deflater.deflate(buffer);
+    byte[] result = new byte[compressedLength];
+    System.arraycopy(buffer, 0, result, 0, compressedLength);
+    return result;
   }
 
   public static String decompress(byte[] source) throws IOException {
@@ -29,7 +28,7 @@ public class Zlib {
       while ((index = input.read(data, 0, data.length)) != -1) {
         buffer.write(data, 0, index);
       }
-      return new String(buffer.toByteArray());
+      return buffer.toString();
     }
   }
 }
