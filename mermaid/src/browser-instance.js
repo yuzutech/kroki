e5d43a287e285c20b7dcae1eff358f8bf84cd9fc
Guillaume Grossetie <ggrossetie@yuzutech.fr>
2022-10-16 16:51:36 +0200

    Capture Chrome process stderr and stdout (#1346)

--- mermaid/src/browser-instance.js
+++ mermaid/src/browser-instance.js
@@ -1,7 +1,10 @@
 const puppeteer = require('puppeteer')
 
+const { logger } = require('./logger')
+
 const createBrowser = async () => {
   const browser = await puppeteer.launch({
+    dumpio: true,
     args: [
       '--disable-dev-shm-usage',
       '--no-first-run',
@@ -9,6 +12,30 @@ const createBrowser = async () => {
       '--no-sandbox',
       '--disable-setuid-sandbox'
     ]
+  })
+
+  const browserProcess = browser.process()
+  browserProcess.stdout.unpipe()
+  browserProcess.stderr.unpipe()
+  browserProcess.stdout.on('data', (data) => {
+    logger.debug({ stdout: data.toString() }, 'chrome process stdout')
+  })
+  browserProcess.stderr.on('data', (data) => {
+    logger.error({ stderr: data.toString() }, 'chrome process')
+  })
+  browserProcess.stdout.resume()
+  browserProcess.stderr.resume()
+  browserProcess.on('disconnect', () => {
+    logger.warn('chrome process disconnected')
+  })
+  browserProcess.on('error', (err) => {
+    logger.error({ err }, 'chrome process errored')
+  })
+  browserProcess.on('exit', (code, signal) => {
+    logger.error({ code, signal }, 'chrome process exited')
+  })
+  browserProcess.on('message', (message) => {
+    logger.warn({ message }, 'chrome process message')
   })
   try {
     return browser
