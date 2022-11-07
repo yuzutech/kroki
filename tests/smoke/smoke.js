7f57cdcd6b1cc9a8d63ca58b3f60398aa8cd0b22
Guillaume Grossetie <ggrossetie@yuzutech.fr>
2022-11-08 00:25:58 +0100

    Test that GraphViz supports jpeg output

--- tests/smoke/smoke.js
+++ tests/smoke/smoke.js
@@ -1,5 +1,5 @@
 const tests = [
-  {engine: 'graphviz', file: 'hello.dot', outputFormat: ['svg']},
+  {engine: 'graphviz', file: 'hello.dot', outputFormat: ['svg', 'jpeg']},
   {engine: 'blockdiag', file: 'kroki.diag', outputFormat: ['svg']},
   {engine: 'seqdiag', file: 'sequence.diag', outputFormat: ['svg']},
   {engine: 'actdiag', file: 'actions.diag', outputFormat: ['svg']},
