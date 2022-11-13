f232f1e1da77a11fdb9d43f0e8ceff15d6b7b352
Guillaume Grossetie <ggrossetie@yuzutech.fr>
2022-11-13 15:12:11 +0100

    troubleshoot smoke test

--- ci/tests/smoke.js
+++ ci/tests/smoke.js
@@ -97,6 +97,7 @@ describe('CJK font', function () {
     const response = await sendRequest(testCase, 'svg')
     try {
       const data = response.body.toString('utf8')
+      console.log({data})
       const boxWidthRegex = /id="flowchart-B-9".*<foreignObject.*width="([0-9.]+)".*ううううううう<\/div><\/foreignObject>/
       expect(data).to.match(boxWidthRegex)
       const match = data.match(boxWidthRegex)
