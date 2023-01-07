b89c72ea26011ccc89eba741531820e82d75092b
Sayak Mukhopadhyay <mukhopadhyaysayak@gmail.com>
2023-01-07 22:53:41 +0530

    feat: allow mermaid to be configured via some query params (#1413)
    Fixes https://github.com/yuzutech/kroki/issues/1326

--- mermaid/src/worker.js
+++ mermaid/src/worker.js
@@ -2,6 +2,7 @@
 const { logger } = require('./logger')
 const path = require('path')
 const puppeteer = require('puppeteer')
+const set = require('lodash/set')
 
 class SyntaxError extends Error {
   constructor () {
@@ -15,12 +16,13 @@ class Worker {
     this.pageUrl = process.env.KROKI_MERMAID_PAGE_URL || `file://${path.join(__dirname, '..', 'assets', 'index.html')}`
   }
 
-  async convert (task) {
+  async convert (task, config) {
     const browser = await puppeteer.connect({
       browserWSEndpoint: this.browserWSEndpoint,
       ignoreHTTPSErrors: true
     })
     const page = await browser.newPage()
+    this.updateConfig(task, config)
     try {
       page.setViewport({ height: 800, width: 600 })
       await page.goto(this.pageUrl)
@@ -64,6 +66,42 @@ class Worker {
         logger.warn({ err }, 'Unable to disconnect from the browser')
       }
     }
+  }
+
+  updateConfig (task, config) {
+    for (const property in Object.fromEntries(config)) {
+      const propertyCamelCase = this.convertPropertyToCamelCase(property)
+      const value = this.getTypedValue(config.get(property))
+      set(task.mermaidConfig, propertyCamelCase, value)
+    }
+  }
+
+  convertPropertyToCamelCase (property) {
+    const propertySplit = property.split('_')
+    for (let i = 0; i < propertySplit.length; i++) {
+      const split = propertySplit[i]
+      const subSplit = split.split('-')
+      if (subSplit.length > 1) {
+        for (let j = 1; j < subSplit.length; j++) {
+          subSplit[j] = subSplit[j].charAt(0).toUpperCase() + subSplit[j].substring(1)
+        }
+        propertySplit[i] = subSplit.join('')
+      }
+    }
+    return propertySplit.join('.')
+  }
+
+  getTypedValue (value) {
+    if (value.toLowerCase() === 'true' || value.toLocaleString() === 'false') {
+      return value === 'true'
+    }
+    if (value.startsWith('[') && value.endsWith(']')) {
+      return value.substring(1, value.length - 1).split(',').map(item => this.getTypedValue(item))
+    }
+    if (!isNaN(value)) {
+      return Number(value)
+    }
+    return value
   }
 }
 
