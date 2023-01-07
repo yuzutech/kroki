df75f671b240b98a44cf62f8023b04836210d74e
Guillaume Grossetie <ggrossetie@yuzutech.fr>
2023-01-07 17:23:56 +0000

    Add Chromium flags for arm64 (#1415)

--- diagrams.net/src/browser-instance.js
+++ diagrams.net/src/browser-instance.js
@@ -3,13 +3,27 @@ const puppeteer = require('puppeteer')
 const createBrowser = async () => {
   const browser = await puppeteer.launch({
     args: [
-      '--disable-dev-shm-usage',
-      '--no-first-run',
-      '--no-initial-navigation',
-      '--no-sandbox',
-      // allow to local files from file://
+      // allow to access files from file:// protocol
+      '--allow-file-access-from-files',
+      // Disables GPU hardware acceleration.
+      // If software renderer is not in place, then the GPU process won't launch.
+      '--disable-gpu',
+      '--disable-translate',
+      // Disable the setuid sandbox (Linux only)
+      '--disable-setuid-sandbox',
+      // disable web security to access local files
       '--disable-web-security',
-      '--allow-file-access-from-files'
+      // Run in headless mode, i.e., without a UI or display server dependencies
+      '--headless',
+      // Prevents creating scrollbars for web content. Useful for taking consistent screenshots.
+      '--hide-scrollbars',
+      // Mutes audio sent to the audio device, so it is not audible during automated testing.
+      '--mute-audio',
+      // Stops new Shell objects from navigating to a default url. ↪
+      '--no-initial-navigation',
+      // Disables the sandbox for all process types that are normally sandboxed.
+      // Meant to be used as a browser-level switch for testing purposes only.
+      '--no-sandbox'
     ]
   })
   try {
--- mermaid/src/browser-instance.js
+++ mermaid/src/browser-instance.js
@@ -5,12 +5,25 @@ const { logger } = require('./logger')
 const createBrowser = async () => {
   const browser = await puppeteer.launch({
     dumpio: true,
+    // reference: https://peter.sh/experiments/chromium-command-line-switches/
     args: [
-      '--disable-dev-shm-usage',
-      '--no-first-run',
+      // Disables GPU hardware acceleration.
+      // If software renderer is not in place, then the GPU process won't launch.
+      '--disable-gpu',
+      '--disable-translate',
+      // Disable the setuid sandbox (Linux only)
+      '--disable-setuid-sandbox',
+      // Run in headless mode, i.e., without a UI or display server dependencies
+      '--headless',
+      // Prevents creating scrollbars for web content. Useful for taking consistent screenshots.
+      '--hide-scrollbars',
+      // Mutes audio sent to the audio device, so it is not audible during automated testing.
+      '--mute-audio',
+      // Stops new Shell objects from navigating to a default url. ↪
       '--no-initial-navigation',
-      '--no-sandbox',
-      '--disable-setuid-sandbox'
+      // Disables the sandbox for all process types that are normally sandboxed.
+      // Meant to be used as a browser-level switch for testing purposes only.
+      '--no-sandbox'
     ]
   })
 
