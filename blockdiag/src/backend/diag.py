26de4e9072e401345d43e71b3bec27c953a550de
Guillaume Grossetie <ggrossetie@yuzutech.fr>
2023-06-14 21:41:55 +0200

    Upgrade to Alpine 3.18 (#1576)

--- blockdiag/src/backend/diag.py
+++ blockdiag/src/backend/diag.py
@@ -59,7 +59,7 @@ def generate_diag(app, diagram_type, output_format, source, options):
 
     app.options.font = [
         '/usr/share/fonts/truetype/dejavu/DejaVuSerif.ttf',
-        '/usr/share/fonts/ttf-dejavu/DejaVuSerif.ttf'
+        '/usr/share/fonts/dejavu/DejaVuSerif.ttf',
     ]
 
     app.fontmap = create_fontmap(app.options)
