453e7f11f0ef77943437dba9f213ec88dba73d01
Nick Douma <nick.douma@bravilor.com>
2026-03-10 13:20:45 +0100

    Re-import app.min.js and shapes-14-6-5.min.js
    These seems to contain references to icons to don't get picked up when
    just syncing the other assets.

--- diagrams.net/import.sh
+++ diagrams.net/import.sh
@@ -11,10 +11,12 @@ fi
 DRAWIODIR=${1}
 ASSETSDIR=${2}
 
-rsync -avz --delete ${DRAWIODIR}/src/main/webapp/export3.html     ${ASSETSDIR}/index.html
-rsync -avz --delete ${DRAWIODIR}/src/main/webapp/export-fonts.css ${ASSETSDIR}/export-fonts.css
-rsync -avz --delete ${DRAWIODIR}/src/main/webapp/images/          ${ASSETSDIR}/images
-rsync -avz --delete ${DRAWIODIR}/src/main/webapp/img/             ${ASSETSDIR}/img
-#rsync -avz --delete ${DRAWIODIR}/src/main/webapp/math4/           ${ASSETSDIR}/math4
-rsync -avz --delete ${DRAWIODIR}/src/main/webapp/mxgraph/         ${ASSETSDIR}/mxgraph
-rsync -avz --delete ${DRAWIODIR}/src/main/webapp/stencils/        ${ASSETSDIR}/stencils
+rsync -avz --delete ${DRAWIODIR}/src/main/webapp/export3.html            ${ASSETSDIR}/index.html
+rsync -avz --delete ${DRAWIODIR}/src/main/webapp/js/app.min.js           ${ASSETSDIR}/js/app.min.js
+rsync -avz --delete ${DRAWIODIR}/src/main/webapp/js/shapes-14-6-5.min.js ${ASSETSDIR}/js/shapes-14-6-5.min.js
+rsync -avz --delete ${DRAWIODIR}/src/main/webapp/export-fonts.css        ${ASSETSDIR}/export-fonts.css
+rsync -avz --delete ${DRAWIODIR}/src/main/webapp/images/                 ${ASSETSDIR}/images
+rsync -avz --delete ${DRAWIODIR}/src/main/webapp/img/                    ${ASSETSDIR}/img
+#rsync -avz --delete ${DRAWIODIR}/src/main/webapp/math4/                  ${ASSETSDIR}/math4
+rsync -avz --delete ${DRAWIODIR}/src/main/webapp/mxgraph/                ${ASSETSDIR}/mxgraph
+rsync -avz --delete ${DRAWIODIR}/src/main/webapp/stencils/               ${ASSETSDIR}/stencils
